e4d8a178d114e1cdb6e4c966c73b19c1a5439f61
Atharv521 <athishinde@gmail.com>
2025-02-15 16:09:27 +0100

    Add files via upload

--- Easy_Queries/Easy_Queries.sql
+++ Easy_Queries/Easy_Queries.sql
@@ -1,42 +1,43 @@
-/* Question Set 1 - Easy */
+----QUESTION SET 2
 
--- Q1. Who is the senior most employee based on job title? 
-SELECT * FROM EMPLOYEE
-ORDER BY LEVELS DESC
-LIMIT 1;
+-- Q1:Write query to return the email, first name, last name, & Genre of all Rock Music
+listeners. Return your list ordered alphabetically by email starting with A
+
+SELECT DISTINCT email, first_name,last_name
+FROM Customer
+JOIN invoice ON Customer.customer_id = invoice.customer_id
+JOIN Invoice_line ON Invoice.invoice_id = Invoice_line.invoice_id
+WHERE track_id IN (
+		SELECT track_id 
+        FROM Track
+		JOIN genre ON track.genre_id = genre.genre_id
+		WHERE genre.name LIKE 'Rock'
+)
+ORDER BY email;
+
+--Q2: Let's invite the artists who have written the most rock music in our dataset. Write a
+query that returns the Artist name and total track count of the top 10 rock bands
+
+SELECT artist.artist_id, artist.name, COUNT(artist.artist_id) AS number_of_songs
+FROM track 
+JOIN album ON album.album_id = track.album_id
+JOIN artist ON album.artist_id = artist.artist_id
+JOIN genre ON genre.genre_id = track.genre_id
+WHERE genre.name  LIKE 'Rock'
+GROUP BY artist.artist_id
+ORDER BY number_of_songs DESC
+LIMIT 10;
 
 
--- Q2. Which countries have the most Invoices?
-SELECT BILLING_COUNTRY, COUNT(*) AS Most_Invoices FROM INVOICE
-GROUP BY BILLING_COUNTRY
-ORDER BY Most_Invoices DESC;
+--Q3: Return all the track names that have a song length longer than the average song length.
+Return the Name and Milliseconds for each track. Order by the song length with the
+longest songs listed first
 
-
--- Q3. What are top 3 values of total invoice?
-SELECT TOTAL FROM INVOICE
-ORDER BY TOTAL DESC
-LIMIT 3;
-
-
--- Q4. Which city has the best customers? 
--- A. We would like to throw a promotional Music Festival in the city we made the most money. 
--- B. Write a query that returns one city that has the highest sum of invoice totals. 
--- C. Return both the city name & sum of all invoice totals.
-SELECT BILLING_CITY, SUM(TOTAL) AS Invoice_Totals FROM INVOICE
-GROUP BY BILLING_CITY
-ORDER BY Invoice_Totals DESC
-LIMIT 1;
-
-
--- Q5. Who is the best customer? 
--- A. The customer who has spent the most money will be declared the best customer. 
--- B. Write a query that returns the person who has spent the most money.
-SELECT C.CUSTOMER_ID, C.FIRST_NAME, C.LAST_NAME, SUM(I.TOTAL) AS TOTAL_AMOUNT
-FROM CUSTOMER AS C
-JOIN INVOICE AS I 
-ON C.CUSTOMER_ID = I.CUSTOMER_ID
-GROUP BY C.CUSTOMER_ID
-ORDER BY TOTAL_AMOUNT DESC
-LIMIT 1;
+SELECT track.name, track.milliseconds
+FROM track
+WHERE milliseconds >
+			(SELECT AVG(milliseconds) AS avg_track_length
+			from track)
+ORDER BY milliseconds DESC;
 
 
